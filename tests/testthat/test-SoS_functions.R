91669abf20be4d986dece7165e086c6b923b349a
AlanCT94 <caciqueta94@gmail.com>
2022-09-28 18:16:51 +0200

    Test cases for all functions

--- tests/testthat/test-SoS_functions.R
+++ tests/testthat/test-SoS_functions.R
@@ -33,3 +33,34 @@ varsndf <- vars[,1:2]
 test_that("Check variables, Select inpatient topic (diagnoserislutenvard)",{
   expect_equal(as.list(varsndf[,2]), list("Region", "Age", "Sex","Measure", "Year", "Diagnos"))
 })
+
+var_cats_obj <- return_meta(type="var_cat", lang="en",
+                            topic="diagnoserislutenvard", var="alder")
+var_cats<- contentToDataframe_meta(var_cats_obj)
+
+var_cats_objr <- return_meta(type="var_cat", lang="en",
+                            topic="diagnoserislutenvard", var="region")
+var_catsr<- contentToDataframe_meta(var_cats_objr)
+
+test_that("Check variable categories, alder and regions",{
+  expect_equal(dim.data.frame(var_cats), c(18,2))
+  expect_equal(var_cats[18,2], "85+")
+  expect_equal(dim.data.frame(var_catsr), c(22,3))
+  expect_equal(var_catsr[1,3], "Entire Sweden")
+})
+
+var_list <- vars[,1]
+values_list <- c("0",
+                 "10,11",
+                 "1,2,3",
+                 "1",
+                 "2017,2018,2019,2020,2021",
+                 "C50,J13")
+df_input_vars <- as.data.frame(cbind(var_list,values_list))
+data_obj <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars)
+data <- contentToDataframe_data(data_obj)
+
+test_that("Check data",{
+  expect_equal(nrow(data), 57)
+  expect_equal(sum(as.numeric(data[,7])), 34796)
+})
