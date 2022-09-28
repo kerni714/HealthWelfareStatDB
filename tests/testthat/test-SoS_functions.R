47a041fbcb29a41a238eb42f0bdb45ed6729af4c
AlanCT94 <caciqueta94@gmail.com>
2022-09-28 19:01:09 +0200

    Variant assignations inside the test_that functions, expect_errors added.

--- tests/testthat/test-SoS_functions.R
+++ tests/testthat/test-SoS_functions.R
@@ -1,66 +1,111 @@
-versions_obj <- return_meta(type="api_version")
-versions<- contentToDataframe_meta(versions_obj)
+
 
 test_that("Check version equal to v1", {
+  versions_obj <- return_meta(type="api_version")
+  versions<- contentToDataframe_meta(versions_obj)
+
   expect_equal(toString(versions[1]), "v1")
   expect_equal(toString(versions[2]), "version 1") #We use to string to compare the values because if not the classesare the ones that are compare
 })
 
-languages_obj <- return_meta(type="lang")
-languages <- contentToDataframe_meta(languages_obj)
+
 
 test_that("Check language is equal to [en]glish and swedish(svenska)",{
+  languages_obj <- return_meta(type="lang")
+  languages <- contentToDataframe_meta(languages_obj)
+
   expect_match(languages[1,1], "sv") #you can use expect_match to compare strings instead of using toString with expect equal
   expect_match(languages[2,2], "engelska")
   expect_equal(dim.data.frame(languages), c(2,2))
   })
 
-topics_objen <- return_meta(type="topic", lang="en")
-topicsen <- contentToDataframe_meta(topics_objen)
-topics_objsv <- return_meta(type="topic", lang="sv")
-topicssv <- contentToDataframe_meta(topics_objsv)
+
 
 test_that("Check topics for english and swedish",{
+  topics_objen <- return_meta(type="topic", lang="en")
+  topicsen <- contentToDataframe_meta(topics_objen)
+  topics_objsv <- return_meta(type="topic", lang="sv")
+  topicssv <- contentToDataframe_meta(topics_objsv)
+
   expect_equal(dim.data.frame(topicsen), c(2,2)) #test length in english
   expect_equal(dim.data.frame(topicssv), c(13,2)) #test length in swedish
   expect_equal((names(topicsen)), c("namn","text"))
 })
 
-vars_obj <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
-vars <- contentToDataframe_meta(vars_obj)
-varsndf <- vars[,1:2]
+
 
 test_that("Check variables, Select inpatient topic (diagnoserislutenvard)",{
-  expect_equal(as.list(varsndf[,2]), list("Region", "Age", "Sex","Measure", "Year", "Diagnos"))
+  vars_obj <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
+  vars <- contentToDataframe_meta(vars_obj)
+  varsndf <- vars[,1:2]
+
+  expect_equal(as.list(varsndf[,1]), list("region", "alder", "kon","matt", "ar", "diagnos"))
 })
 
-var_cats_obj <- return_meta(type="var_cat", lang="en",
-                            topic="diagnoserislutenvard", var="alder")
-var_cats<- contentToDataframe_meta(var_cats_obj)
 
-var_cats_objr <- return_meta(type="var_cat", lang="en",
-                            topic="diagnoserislutenvard", var="region")
-var_catsr<- contentToDataframe_meta(var_cats_objr)
 
 test_that("Check variable categories, alder and regions",{
+  var_cats_obj <- return_meta(type="var_cat", lang="en",
+                              topic="diagnoserislutenvard", var="alder")
+  var_cats<- contentToDataframe_meta(var_cats_obj)
+
+  var_cats_objr <- return_meta(type="var_cat", lang="en",
+                               topic="diagnoserislutenvard", var="region")
+  var_catsr<- contentToDataframe_meta(var_cats_objr)
+
   expect_equal(dim.data.frame(var_cats), c(18,2))
-  expect_equal(var_cats[18,2], "85+")
+  expect_equal(var_cats[18,1], 18)
   expect_equal(dim.data.frame(var_catsr), c(22,3))
-  expect_equal(var_catsr[1,3], "Entire Sweden")
+  expect_equal(var_catsr[1,1], 0)
 })
 
-var_list <- vars[,1]
-values_list <- c("0",
-                 "10,11",
-                 "1,2,3",
-                 "1",
-                 "2017,2018,2019,2020,2021",
-                 "C50,J13")
-df_input_vars <- as.data.frame(cbind(var_list,values_list))
-data_obj <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars)
-data <- contentToDataframe_data(data_obj)
+
 
 test_that("Check data",{
-  expect_equal(nrow(data), 57)
-  expect_equal(sum(as.numeric(data[,7])), 34796)
+  vars_obj <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
+  vars <- contentToDataframe_meta(vars_obj)
+  var_list <- vars[,1]
+  values_list <- c("0",
+                   "10,11",
+                   "1,2,3",
+                   "1",
+                   "2017,2018,2019,2020,2021",
+                   "C50,J13")
+  df_input_vars <- as.data.frame(cbind(var_list,values_list))
+  data_obj <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars)
+  data <- contentToDataframe_data(data_obj)
+
+  expect_equal(dim.data.frame(data), c(57,7))
+})
+
+test_that("return_data() discovers erroneous value input, number", {
+  vars_obj <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
+  vars <- contentToDataframe_meta(vars_obj)
+  var_list <- vars[,1]
+  values_list <- c("0",
+                   "10",
+                   "1",
+                   "1",
+                   "2032,2013",
+                   "J13")
+  df_input_vars <- as.data.frame(cbind(var_list,values_list))
+  #data_obj <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars)
+
+  expect_error(return_data(lang="en",topic="diagnoserislutenvard", df_input_vars))
+})
+
+test_that("return_data() discovers erroneous value input, character", {
+  vars_obj <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
+  vars <- contentToDataframe_meta(vars_obj)
+  var_list <- vars[,1]
+  values_list <- c("0",
+                   "10",
+                   "1",
+                   "1",
+                   "2012,2013",
+                   "V99")
+  df_input_vars <- as.data.frame(cbind(var_list,values_list))
+  #data_obj <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars)
+
+  expect_error(return_data(lang="en",topic="diagnoserislutenvard", df_input_vars))
 })
