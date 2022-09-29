6fde24958e46bca687722f57681dc2faafafceec
AlanCT94 <caciqueta94@gmail.com>
2022-09-29 21:34:29 +0200

    Comments cleaned

--- Shiny/AppHealthWelfareStatDB.R
+++ Shiny/AppHealthWelfareStatDB.R
@@ -1,18 +1,10 @@
 library(shiny)
 library(ggplot2)
 
-# Data pre-processing ----
-# Tweak the "am" variable to have nicer factor labels -- since this
-# doesn't rely on any user inputs, we can do this once at startup
-# and then use the value throughout the lifetime of the app
+# Data pre-processing
+
 vars <- return_meta(type="var", lang="en", topic="diagnoserislutenvard")
 var_list <- vars[,1]
-# values_list <- c("1,3",
-#                  "10,11",
-#                  "1,2",
-#                  "1",
-#                  "2012,2013",
-#                  "J13")
 
 values_list <- c("1,3",
                  "10",
@@ -25,48 +17,32 @@ values_list <- c("1,3",
 df_input_vars <- as.data.frame(cbind(var_list,values_list))
 data <- return_data(lang="en",topic="diagnoserislutenvard", df_input_vars, addText=TRUE)
 
-#data_all <- contentToDataframe_data(data_obj)
-#data <- data_all[,2:6]
-
-#data <- contentToDataframe_data(data_obj, addText=TRUE)
-
-
-
-# Define UI for miles per gallon app ----
+# Define UI
 ui <- fluidPage(
 
   # App title ----
   titlePanel("Inpatient care diagnosis data"),
 
-  # Sidebar layout with input and output definitions ----
+  # Sidebar layout with input and output
   sidebarLayout(
 
-    # Sidebar panel for inputs ----
+    # Sidebar panel for inputs
     sidebarPanel(
 
-      ##############
-      #radioButtons("radio", label = ("Radio buttons"),
-      #             choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-      #             selected = 1),
 
-      #hr(),
-      #fluidRow(column(3, verbatimTextOutput("value"))),
-
-      ##############
-
-      # Input: Selector for variable to plot against mpg ----
+      # Input: Selector for variable to plot
       selectInput("var1", "X_axis_variable",
                   c("Year" = "ar",
                     "Sex" = "konText",
                     "Region" = "regionText")),
 
-      # Input: Selector for variable to plot against mpg ----
+      # Input: Selector for variable to plot
       selectInput("var2", "Stack variable:",
                   c("Sex" = "konText",
                     "Year" = "ar",
                     "Region" = "regionText")),
 
-      # Input: Selector for variable to plot against mpg ----
+      # Input: Selector for variable to plot
       selectInput("var3", "Trellis variable:",
                   c("Region" = "regionText",
                     "Sex" = "konText",
@@ -76,24 +52,22 @@ ui <- fluidPage(
     # Main panel for displaying outputs ----
     mainPanel(
 
-      # Output: Formatted text for caption ----
+      # Output: Formatted text for caption
       h3(textOutput("caption")),
 
-      # Output: Plot of the requested variable against mpg ----
+      # Output: Plot of the requested variable
       plotOutput("plot")
 
     )
   )
 )
 
-# Define server logic to plot various variables against mpg ----
+# Define server logic to plot various variables
 server <- function(input, output) {
 
-  # Compute the formula text ----
-  # This is in a reactive expression since it is shared by the
-  # output$caption and output$mpgPlot functions
+ #Reactive part of the program
   formulaText <- reactive({
-    #paste("mattId ~", input$var1, input$var2, input$var3)
+
     measure <- unique(data$mattText)
     paste(measure)
   })
@@ -103,21 +77,16 @@ server <- function(input, output) {
     formulaText()
   })
 
-  # Generate a plot of the requested variable against mpg ----
-  # and only exclude outliers if requested
+#Plotting
   output$plot <- renderPlot({
     df <- data
-    #df$in1 <- input$var1
-    #df$in2 <- input$var2
-    #df$in3 <- input$var3
+
 
     var1 <- data[,input$var1]
     var2 <- data[,input$var2]
     var3 <- data[,input$var3]
     varde <- data[,"varde"]
     data$var3 <- var3
-    #ggplot(data, aes(fill=var2, x=var1, y=as.numeric(varde))) +
-    #  geom_bar(position="stack", stat="identity") + facet_wrap(~ var3)
 
 
 
@@ -133,11 +102,6 @@ server <- function(input, output) {
       geom_bar(position="stack", stat="identity") + facet_wrap(~ var3)+
       xlab(namevar1) + ylab("Length of stay") + labs(fill = namevar2)
 
-    #ggplot(df, aes(fill=in2, x=in1, y=as.numeric(varde))) +
-    #  geom_bar(position="stack", stat="identity") + facet_wrap(~ in3)
-
-    #ggplot(data, aes(fill=konText, x=ar, y=as.numeric(varde))) +
-    #  geom_bar(position="stack", stat="identity") + facet_wrap(~ regionText)
 
   })
 
